d8b99200bcb27409921ea2a3ecf5076630b5cabd
brkypc <berkayyapici1998@gmail.com>
2021-05-08 21:42:34 +0300

    sensor - fall detection

--- app/src/main/java/tr/edu/yildiz/berkayyapici/MenuActivity.java
+++ app/src/main/java/tr/edu/yildiz/berkayyapici/MenuActivity.java
@@ -4,16 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.util.List;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
     RelativeLayout questionsList, addQuestion, examsList, createExam;
     TextView welcomeText;
     ImageView logout;
+
+    SensorManager sm = null;
+    List list;
+
+    SensorEventListener sel = new SensorEventListener(){
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+        public void onSensorChanged(SensorEvent event) {
+
+            float loX = event.values[0];
+            float loY = event.values[1];
+            float loZ = event.values[2];
+
+            double loAccelerationReader = Math.sqrt(Math.pow(loX, 2)
+                    + Math.pow(loY, 2)
+                    + Math.pow(loZ, 2));
+
+            if (loAccelerationReader > 0.3 && loAccelerationReader < 0.5) {
+                Toast.makeText(MenuActivity.this, "You dropped your phone :)", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +52,23 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_menu);
         defineVariables();
         defineListeners();
+
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        list = sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if(list.size()>0){
+            sm.registerListener(sel, (Sensor) list.get(0), SensorManager.SENSOR_DELAY_NORMAL);
+        }else{
+            Toast.makeText(MenuActivity.this, "Error: No Accelerometer.", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(list.size()>0){
+            sm.unregisterListener(sel);
+        }
     }
 
     private void defineVariables() {

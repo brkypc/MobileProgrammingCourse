14b7e592fe8eab3dcf412edadae0766bff98d872
brkypc <berkayyapici1998@gmail.com>
2021-05-16 15:12:15 +0300

    final

--- app/src/main/java/tr/edu/yildiz/berkayyapici/Question.java
+++ app/src/main/java/tr/edu/yildiz/berkayyapici/Question.java
@@ -1,14 +1,15 @@
 package tr.edu.yildiz.berkayyapici;
 
-import java.util.ArrayList;
-
 public class Question {
-    private String question, answer, choice1, choice2, choice3, choice4, uri;
+    private final String question;
+    private final String answer;
+    private final String choice1;
+    private final String choice2;
+    private final String choice3;
+    private final String choice4;
+    private final String uri;
     private int id;
 
-    public Question() {
-
-    }
     public Question(String question, String answer, String choice1, String choice2, String choice3, String choice4, String uri) {
         this.question = question;
         this.answer = answer;
@@ -58,15 +59,4 @@ public class Question {
 
     public String getUri() { return uri; }
 
-    public ArrayList<Question> defaultQuestions() {
-        ArrayList<Question> questionsList = new ArrayList<>();
-
-        questionsList.add(new Question("What is a correct syntax to output \"Hello World\" in Java?","System.out.println(\"Hello World\");",
-                "print(\"Hello World\");", "printf(\"Hello World\")", "echo(\"Hello World\")", "Console.WriteLine(\"Hello World\")", "none#none#none"));
-        questionsList.add(new Question("Which method can be used to find the length of a string in Java?","length()",
-                "strlen()", "size()", "getSize()", "len()", "none#none#none"));
-        questionsList.add(new Question("How to stop the services in android?","stopSelf()",
-                "finish()", "System.exit()", "kill()", "delete()", "none#none#none"));
-        return questionsList;
-    }
 }
